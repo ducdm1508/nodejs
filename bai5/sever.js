5c826af36a13114caf22a0bca499a7ec405ba5a6
ducdm1508 <duc.dm.2442@aptechlearning.edu.vn>
2025-07-04 10:17:39 +0700

    Update sever.js

--- bai5/sever.js
+++ bai5/sever.js
@@ -3,8 +3,6 @@ const app = express();
 const port = 3000;
 const mySql = require('mysql2');
 const fs = require('fs');
-const { log } = require('console');
-const { title } = require('process');
 
 const logStream = fs.createWriteStream('log.txt', { flags: 'a' });
 
